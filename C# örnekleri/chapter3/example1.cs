23ddcb8116fcd53417fc8e7dda6449b80c7637e4
azim7jje32 <azimesapanca59@gmail.com>
2025-11-30 22:06:12 +0300

    Add files via upload

---
+++ C# örnekleri/chapter3/example1.cs
@@ -0,0 +1,14 @@
+﻿// Fig. 3.1: Welcome1.cs
+// Text-displaying app.
+using System;
+ 
+class Welcome1
+{
+    // Main method begins execution of C# app
+    static void Main()
+    {
+        string person = "Paul"; // variable that stores the string "Paul"
+        Console.WriteLine($"Welcome to C# Programming, {person}!");
+    }
+    // end Main
+ } // end class Welcome1
---
+++ C# örnekleri/chapter7/example3.cs
@@ -0,0 +1,29 @@
+﻿// Fig. 7.17: FactorialTest.cs
+// Recursive Factorial method.
+using System;
+
+class FactorialTest
+{
+    static void Main()
+    {
+        // calculate the factorials of 0 through 10
+        for (long counter = 0; counter <= 10; ++counter)
+        {
+            Console.WriteLine($"{counter}! = {Factorial(counter)}");
+        }
+    }
+
+    // recursive declaration of method Factorial  
+    static long Factorial(long number)
+    {
+        // base case                               
+        if (number <= 1)
+        {
+            return 1;
+        }
+        else // recursion step 
+        {
+            return number * Factorial(number - 1);
+        }
+    }
+}
