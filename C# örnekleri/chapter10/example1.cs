422c215d2e33c19e9e02834d1e7a03c2a7add16b
azim7jje32 <azimesapanca59@gmail.com>
2025-12-02 11:34:31 +0300

    Add files via upload

---
+++ C# örnekleri/chapter10/example1.cs
@@ -0,0 +1,41 @@
+﻿// Fig. 10.1: Time1.cs
+// Time1 class declaration maintains the time in 24-hour format.
+using System; // namespace containing ArgumentOutOfRangeException
+
+public class Time1
+{
+    private int hour; // 0 - 23
+    private int minute; // 0 - 59
+    private int second; // 0 - 59
+
+    // set a new time value using universal time; throw an 
+    // exception if the hour, minute or second is invalid
+    public void SetTime(int h, int m, int s)
+    {
+        // validate hour, minute and second
+        if ((h >= 0 && h < 24) && (m >= 0 && m < 60) &&
+           (s >= 0 && s < 60))
+        {
+            hour = h;
+            minute = m;
+            second = s;
+        } // end if
+        else
+            throw new ArgumentOutOfRangeException();
+    } // end method SetTime
+
+    // convert to string in universal-time format (HH:MM:SS)
+    public string ToUniversalString()
+    {
+        return string.Format("{0:D2}:{1:D2}:{2:D2}",
+           hour, minute, second);
+    } // end method ToUniversalString
+
+    // convert to string in standard-time format (H:MM:SS AM or PM)
+    public override string ToString()
+    {
+        return string.Format("{0}:{1:D2}:{2:D2} {3}",
+           ((hour == 0 || hour == 12) ? 12 : hour % 12),
+           minute, second, (hour < 12 ? "AM" : "PM"));
+    } // end method ToString
+} // end class Time1
---
+++ C# örnekleri/chapter6/example1.cs
@@ -0,0 +1,19 @@
+﻿// Fig. 6.5: Sum.cs
+// Summing integers with the for statement.
+using System;
+
+class Sum
+{
+   static void Main()
+   {
+       int total = 0; // initialize total
+
+       //total even integers from 2 through 20
+       for (int number = 2; number <= 20; number += 2)
+       {
+           total += number;
+       }
+
+       Console.WriteLine($"Sum is {total}"); // display results
+   }
+}
